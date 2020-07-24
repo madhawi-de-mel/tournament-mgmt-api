6671dba5ad1a7c03a1f41099cfbeae6c3bdda96b
Madhawi De Mel <madhawi.demel@gmail.com>
2020-07-24 21:26:21 +0530

    Adding tests for percentile

--- management_app/tests.py
+++ management_app/tests.py
@@ -1,22 +1,46 @@
-import unittest
-
+# import unittest
+#
 from django.test import TestCase
-
-from management_app.services.player_detail_service import PlayerDetailsService
+#
+#
+from management_app.models import Team, Player
+from management_app.services.player_detail_service import get_best_players, set_team_average, set_player_average_score
 
 
 class PlayerDetailsTest(TestCase):
+    fixtures = ['tournament.json']
 
-    def test_get_all_player_details(self):
-        players = PlayerDetailsService.get_all_player_details()
-        self.assertIsNotNone(players)
-        self.assertEqual(len(players), 2)
+    def setUp(self):
+        set_team_average()
+        set_player_average_score()
 
-    def test_get_player_details(self):
-        player = PlayerDetailsService.get_player_details()
-        self.assertIsNotNone(player)
-        self.assertEqual(player.name, "Andrew John")
+    #     def test_get_all_player_details(self):
+    #         players = get_all_player_details()
+    #         self.assertIsNotNone(players)
+    #         self.assertEqual(len(players), 2)
+    #
+    #     def test_get_player_details(self):
+    #         player = PlayerDetailsService.get_player_details()
+    #         self.assertIsNotNone(player)
+    #         self.assertEqual(player.name, "Andrew John")
+    #
+    #
+    #
+    # if __name__ == '__main__':
+    #     unittest.main()
 
+    def test_get_best_player_ninetieth(self):
+        best_players = get_best_players(1, 90)
+        self.assertIsNotNone(best_players)
+        self.assertEquals(len(best_players), 1)
+        self.assertEquals(best_players[0].name, 'Riane James')
+        assert True
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_get_best_player_tenth(self):
+        best_players = get_best_players(1, 10)
+        self.assertIsNotNone(best_players)
+        self.assertEquals(len(best_players), 3)
+        # check if sort is working
+        self.assertTrue(best_players[0].average_score >= best_players[1].average_score)
+        self.assertTrue(best_players[1].average_score >= best_players[2].average_score)
+        assert True
